82a2d6b85a3032858aa5c69ab71ffdb684c6e48c
klemens <klemens.raduner@nyp.ch>
2020-08-26 11:51:26 +0200

    register

---
+++ register.php
@@ -0,0 +1,20 @@
+<?php
+
+include 'db_connection.php';
+
+if (!empty($_POST)) {
+
+    $query = $conn->prepare("INSERT INTO users (username, password, salt) VALUES (?, ?, ?)");
+
+    $username = $_POST["username"];
+    $salt = uniqid(mt_rand(), true);
+    $hashedpw = password_hash($_POST["password"] . $salt, PASSWORD_BCRYPT);
+
+    $query->bind_param('sss', $username, $hashedpw, $salt);
+    $query->execute();
+
+    header('Location:index.php?=registered', true, $statusCode);
+} else {
+    header('Location:index.php', true, $statusCode);
+    die();
+}
